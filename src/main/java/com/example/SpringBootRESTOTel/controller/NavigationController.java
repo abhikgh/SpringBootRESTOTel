b0682cf03cfb3a77db9e4b22578074f7e9dad7db
Abhik <abhikgh.work@gmail.com>
2023-11-28 22:16:02 +0530

    updates

--- src/main/java/com/example/SpringBootRESTOTel/controller/NavigationController.java
+++ src/main/java/com/example/SpringBootRESTOTel/controller/NavigationController.java
@@ -14,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/orders/v1")
 @Slf4j
 public class NavigationController {
+
+    private Logger logger = Logger.getLogger(NavigationController.class.getName());
 
     @Autowired
     private MovieService movieService;
